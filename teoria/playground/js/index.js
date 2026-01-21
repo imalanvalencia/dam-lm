169d51532f37df6660f1c173de702910e38f4b8e
Alan Valencia <alavalizq@alu.edu.gva.es>
2026-01-21 09:07:11 +0100

    feat: clase de if, blucles y functions

--- teoria/playground/js/index.js
+++ teoria/playground/js/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  console.log(e, "Cargo el DOM");
+  console.log(e, "Cargo el DOM")
+  let $body = document.querySelector("body")
+  let $form_nota = document.querySelector("#nota")
+  let $result_nota = document.querySelector("#result-nota")
+  let $form_adivina = document.querySelector("#adivina")
+  let $result_adivina = document.querySelector("#result-adivina")
+  let $inferior = document.querySelector("#inferior")
+  let $superior = document.querySelector("#superior")
 
   let tabla = [
     ["cafe", "precio"],
@@ -11,24 +18,62 @@ document.addEventListener("DOMContentLoaded", (e) => {
     ["americano", "10.2"],
     ["americano", "10.2"],
     ["americano", "10.2"],
-  ];
+  ]
 
-  let $button = document.querySelector("button");
-  let $input = document.querySelector("input");
-  let $text = document.querySelector("p");
-  let $body = document.querySelector("body");
+  $body.insertAdjacentHTML(
+    "beforeend",
+    `<table border="3"> ${tabla.map((tr) => `<tr>${tr.map((t) => `<td>${t}</td>`)}</tr>`)} <table>`,
+  )
 
-  $body.innerHTML += `<table border="3"> ${tabla.map((tr) => `<tr>${tr.map((t) => `<td>${t}</td>`)}</tr>`)} <table>`;
+  $form_nota.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-  $button.addEventListener("click", (e) => {
-    console.log($input.value, $input.value.length);
+    let InputValue = parseInt(e.target[0].value)
 
-    if ($input.value > "10") $text.innerText = "muy alta";
-    if ($input.value > "8") $text.innerText = "EXCELENTE";
-    if ($input.value > "6") $text.innerText = "SUFICIENTE";
-    if ($input.value > "5") $text.innerText = "CASI PIERDES";
-    if ($input.value > "3") $text.innerText = "MALA NOTA";
-    if ($input.value > "0") $text.innerText = "RENUNCIA";
-    else  $text.innerText = "MUY BAJA";
-  });
-});
+    if (InputValue > 10) $result_nota.innerText = "muy alta"
+    else if (InputValue > 8) $result_nota.innerText = "EXCELENTE"
+    else if (InputValue > 6) $result_nota.innerText = "SUFICIENTE"
+    else if (InputValue > 5) $result_nota.innerText = "CASI PIERDES"
+    else if (InputValue > 3) $result_nota.innerText = "MALA NOTA"
+    else if (InputValue > 0) $result_nota.innerText = "RENUNCIA"
+    else $result_nota.innerText = "MUY BAJA"
+  })
+
+  let max = 10
+  let min = 1
+  let intentos = 0
+  let $label_adivina = document.querySelector("#label-adivina")
+  let $input_adivina = $form_adivina.querySelector("input")
+
+  let aleatorio = Math.floor(Math.random() * (max - min + 1)) + min
+
+  $superior.innerText = max
+  $inferior.innerText = min
+
+  console.log(aleatorio)
+  
+  const generarNuevo = () => {
+    aleatorio = Math.floor(Math.random() * (max - min + 1)) + min
+    intentos = 0
+    console.log(aleatorio)
+  }
+  
+  $form_adivina.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    intentos++
+    let InputValue = parseInt(e.target[0].value)
+
+    if (InputValue > aleatorio)
+      $result_adivina.innerText = "Numero es mas alto"
+    else if (InputValue < aleatorio)
+      $result_adivina.innerText = "Numero es mas bajo"
+    else if (InputValue === aleatorio) {
+      alert(`Has adivinado en ${intentos} intentos!`)
+      $result_adivina.innerText = ""
+      $label_adivina.innerHTML = `Adivina otro numero entre <span id="inferior">${min}</span>  hasta <span id="superior">${max}</span>`
+      $input_adivina.value = ""
+      generarNuevo()
+    }
+  })
+})
